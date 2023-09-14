737e817a1ddf451abc20a2af635b41889b401a9d
Herberth3 <ibiem3@gmail.com>
2023-09-14 17:49:57 -0600

    [ADD] login service with http implemented

--- frontend/src/app/components/dashboard-login/dashboard-login.component.ts
+++ frontend/src/app/components/dashboard-login/dashboard-login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from "@angular/router";
+import { LoginService } from "../../services/login-service";
 
 @Component({
   selector: 'app-dashboard-login',
@@ -10,9 +12,33 @@ export class DashboardLoginComponent {
   nickname: string = '';
   password: string = '';
 
-  iniciarSesion(crearForm: NgForm): void{
+  constructor(private authService: LoginService, private _router: Router) { } // Inyecta el servicio AuthService
+
+  iniciarSesion(crearForm: NgForm): void {
     this.nickname = crearForm.value.nickname;
     this.password = crearForm.value.password;
+
+    // Llama al método login del servicio LoginService
+    this.authService.login(this.nickname, this.password).subscribe(
+      res => {
+        var respuesta: string = res["idtype"];
+        console.log("respuesta" + respuesta);
+        if (respuesta == "1") {
+          this._router.navigate(['admin']);
+        } else if (respuesta == "2") {
+          this._router.navigate(['profesores']);
+        } else if (respuesta == "3") {
+          this._router.navigate(['alumnos']);
+        } else {
+          alert("Usuario o contraseña no valido");
+          crearForm.reset();
+          this._router.navigate(['login']);
+
+        }
+      }, (error) => {
+        console.error(error);
+      }
+    )
 
     console.log(this.nickname);
     console.log(this.password);
---
+++ frontend/src/app/services/login-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private apiUrl = 'http://localhost:3000'; // Reemplaza con la URL de tu API
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { username, password });
+  }
+}
