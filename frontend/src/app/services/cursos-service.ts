cc747c2b57423fca5fa895648d7170a95e4ea046
Herberth3 <ibiem3@gmail.com>
2023-09-14 20:42:21 -0600

    [UPDATE] teacher courses service implemented

---
+++ frontend/src/app/services/cursos-service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Curso } from '../models/curso-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+  private apiUrl = 'http://localhost:3000'; // Reemplaza con la URL de tu API
+
+  constructor(private http: HttpClient) { }
+
+  getCursos(id_profesor: number): Observable<Curso[]> {
+    return this.http.get<Curso[]>(`${this.apiUrl}/cursos/` + id_profesor);
+  }
+}
